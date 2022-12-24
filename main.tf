4d78fcf18836573b8ae202f1fd694097e3bd21fc
Aaron Reisman <aaron@reisman.org>
2022-12-23 19:31:05 -0600

    Don't specify provider block in module

--- main.tf
+++ main.tf
@@ -3,13 +3,7 @@ terraform {
     proxmox = {
       source = "Telmate/proxmox"
       version = "2.9.11"
+      configuration_aliases = [ proxmox ]
     }
   }
-}
-
-provider "proxmox" {
-  pm_api_url = format("https://%s:%d/api2/json", var.proxmox_host, var.proxmox_port)
-  pm_parallel = 1
-  pm_user = var.proxmox_user
-  pm_password = var.proxmox_password
 }
