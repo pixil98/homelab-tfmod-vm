bd15fd4542a8be526c76348dd3268ff992e94b5a
Aaron Reisman <aaron@reisman.org>
2022-12-26 00:16:18 -0600

    Don't quote the config aliases

--- main.tf
+++ main.tf
@@ -3,12 +3,12 @@ terraform {
   required_providers {
     null = {
       version = "3.2.1"
-      configuration_aliases = [ "null" ]
+      configuration_aliases = [ null ]
     }
     proxmox = {
       source = "Telmate/proxmox"
       version = "2.9.11"
-      configuration_aliases = [ "proxmox" ]
+      configuration_aliases = [ proxmox ]
     }
   }
 }
