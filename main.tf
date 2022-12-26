a64467a7d450087d8cb223ec8a96428f2d0c2209
Aaron Reisman <aaron@reisman.org>
2022-12-26 00:20:38 -0600

    Add missing brace, remove configuration_aliases

--- main.tf
+++ main.tf
@@ -4,10 +4,9 @@ terraform {
     proxmox = {
       source = "Telmate/proxmox"
       version = "2.9.11"
-      configuration_aliases = [ proxmox ]
     }
     null = {
       version = "3.2.1"
-      configuration_aliases = [ null ]
     }
+  }
 }
