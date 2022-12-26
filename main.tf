bc2eb74477925c6c19941a761ce24facab114f3f
Aaron Reisman <aaron@reisman.org>
2022-12-26 00:17:13 -0600

    Swap provider order

--- main.tf
+++ main.tf
@@ -1,14 +1,13 @@
 terraform {
   required_version = ">= 1.3.6"
   required_providers {
-    null = {
-      version = "3.2.1"
-      configuration_aliases = [ null ]
-    }
     proxmox = {
       source = "Telmate/proxmox"
       version = "2.9.11"
       configuration_aliases = [ proxmox ]
     }
-  }
+    null = {
+      version = "3.2.1"
+      configuration_aliases = [ null ]
+    }
 }
