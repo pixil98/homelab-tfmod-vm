5d39b4d482bf1839e3fd52742c68f0a63812cba1
Aaron Reisman <aaron@reisman.org>
2023-12-25 08:33:20 -0600

    Update terraform providers

--- main.tf
+++ main.tf
@@ -3,14 +3,15 @@ terraform {
   required_providers {
     proxmox = {
       source = "bpg/proxmox"
-      version = "0.38.1"
+      version = "0.42.0"
     }
     null = {
+      source = "hashicorp/null"
       version = "3.2.2"
     }
     tls = {
       source = "hashicorp/tls"
-      version = "4.0.4"
+      version = "4.0.5"
     }
   }
 }
