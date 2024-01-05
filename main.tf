d1586d4a83c988421669950a243703c83532f200
Aaron Reisman <aaron@reisman.org>
2024-01-04 20:34:49 -0600

    Update bgp/proxmox provider to 0.43.0

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 terraform {
   required_version = ">= 1.3.6"
   required_providers {
-    proxmox = {
-      source = "bpg/proxmox"
-      version = "0.42.0"
-    }
     null = {
       source = "hashicorp/null"
       version = "3.2.2"
+    }
+    proxmox = {
+      source = "bpg/proxmox"
+      version = "0.43.0"
     }
     tls = {
       source = "hashicorp/tls"
