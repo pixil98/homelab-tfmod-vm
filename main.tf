1f473912c79590cd9c5b3c819fbd0ab4c2200489
Aaron Reisman <areisman@epic.com>
2024-11-22 15:13:48 -0600

    fix linter errors

--- main.tf
+++ main.tf
@@ -2,15 +2,15 @@ terraform {
   required_version = ">= 1.3.6"
   required_providers {
     null = {
-      source = "hashicorp/null"
+      source  = "hashicorp/null"
       version = "3.2.3"
     }
     proxmox = {
-      source = "bpg/proxmox"
+      source  = "bpg/proxmox"
       version = "0.67.1"
     }
     tls = {
-      source = "hashicorp/tls"
+      source  = "hashicorp/tls"
       version = "4.0.6"
     }
   }
