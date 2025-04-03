a196475b0944698f640d61109413d445f75ef886
Aaron Reisman <aaron@reisman.org>
2025-04-03 01:00:07 -0500

    Add physical_host output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "ip_address" {
   description = "Virtual machine IP address"
   value       = var.vm_network_address
 }
+
+output "physical_host" {
+  description = "The physical host on which the VM is provisioned."
+  value       = var.node
+}
